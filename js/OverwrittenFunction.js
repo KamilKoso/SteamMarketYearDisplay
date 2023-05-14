302cd7f4b213f876b25465af2d69c18cb8ec1316
KamilKoso <kamilkosobudzki1@gmail.com>
2023-05-14 22:09:34 +0200

    added barRenderer

--- js/OverwrittenFunction.js
+++ js/OverwrittenFunction.js
@@ -1,46 +1,56 @@
-function CreatePriceHistoryGraph(line1, numYAxisTicks, strFormatPrefix, strFormatSuffix )
-{
-    var plot = $J.jqplot('pricehistory', [line1], {
-        title:{text: 'Median Sale Prices', textAlign: 'left' },
-        gridPadding:{left: 45, right:45, top:25},
-        axesDefaults:{ showTickMarks:false },
-        axes:{
-            xaxis:{
-                renderer:$J.jqplot.DateAxisRenderer,
-                tickOptions:{formatString:'%b %#d %Y<span class="priceHistoryTime"> %#I%p<span>'},
-                pad: 1
-            },
-            yaxis: {
-                pad: 1.1,
-                tickOptions:{formatString:strFormatPrefix + '%0.2f' + strFormatSuffix, labelPosition:'start', showMark: false},
-                numberTicks: numYAxisTicks
-            }
-        },
-        grid: {
-            gridLineColor: '#1b2939',
-            borderColor: '#1b2939',
-            background: '#101822'
-        },
-        cursor: {
-            show: true,
-            zoom: true,
-            showTooltip: false
-        },
-        highlighter: {
-            show: true,
-            lineWidthAdjust: 2.5,
-            sizeAdjust: 5,
-            showTooltip: true,
-            tooltipLocation: 'n',
-            tooltipOffset: 20,
-            fadeTooltip: true,
-            yvalues: 2,
-            formatString: '<strong>%s</strong><br>%s<br>%d sold'
-        },
-        series:[{lineWidth:3, markerOptions:{show: false, style:'circle'}}],
-        seriesColors: [ "#688F3E" ]
-    });
+function CreatePriceHistoryGraph(line1, numYAxisTicks, strFormatPrefix, strFormatSuffix) {
+  var plot = $J.jqplot("pricehistory", [line1, line1.map(x => [x[0], x[2]])], {
+    title: { text: "Median Sale Prices", textAlign: "left" },
+    gridPadding: { left: 45, right: 45, top: 25 },
+    axesDefaults: { showTickMarks: false },
+    axes: {
+      xaxis: {
+        renderer: $J.jqplot.DateAxisRenderer,
+        tickOptions: { formatString: '%b %#d %Y<span class="priceHistoryTime"> %#I%p<span>' },
+        pad: 1,
+      },
+      yaxis: {
+        pad: 1.1,
+        tickOptions: { formatString: strFormatPrefix + "%0.2f" + strFormatSuffix, labelPosition: "start", showMark: false },
+        numberTicks: numYAxisTicks,
+      },
+      y2axis: {
+        label: "Volume",
+        tickOptions:{showGridline:false},
+        min: 0,
+        renderer: $J.jqplot.BarRenderer,
+      },
+    },
+    grid: {
+      gridLineColor: "#1b2939",
+      borderColor: "#1b2939",
+      background: "#101822",
+    },
+    cursor: {
+      show: true,
+      zoom: true,
+      showTooltip: false,
+    },
+    highlighter: {
+      show: true,
+      lineWidthAdjust: 2.5,
+      sizeAdjust: 5,
+      showTooltip: true,
+      tooltipLocation: "n",
+      tooltipOffset: 20,
+      fadeTooltip: true,
+      yvalues: 2,
+      formatString: "<strong>%s</strong><br>%s<br>%d sold",
+    },
+    series: [
+      { lineWidth: 3, markerOptions: { show: false, style: "circle" }, yaxis: "yaxis" },
+      { label: "Volume", renderer: $J.jqplot.BarRenderer, yaxis: "y2axis", rendererOptions: {barWidth: 25} },
+    ],
+    seriesColors: ["#688F3E", "#B3DFFF"],
+  });
 
-    plot.defaultNumberTicks = numYAxisTicks;
-    return plot;
+  plot.defaultNumberTicks = numYAxisTicks;
+
+  console.log(plot);
+  return plot;
 }
--- js/SteamMarketListingsInjector.js
+++ js/SteamMarketListingsInjector.js
@@ -1,22 +1,29 @@
 var observer = new MutationObserver(function (mutations) {
-    mutations.forEach(function (mutation) {
-        if (!mutation.addedNodes) return
-        for (let i = 0; i < mutation.addedNodes.length; i++) {
-            let node = mutation.addedNodes[i];
-            if (node.tagName === "SCRIPT") {
-                if (node.getAttribute("src") != null && node.getAttribute("src").includes("economy.js")) {
-                    let element = document.createElement("script");
-                    element.setAttribute("src", chrome.runtime.getURL("js/OverwrittenFunction.js"));
-                    document.head.appendChild(element);
-                }
-            }
+  mutations.forEach(function (mutation) {
+    if (!mutation.addedNodes) return;
+    for (let i = 0; i < mutation.addedNodes.length; i++) {
+      let node = mutation.addedNodes[i];
+      if (node.tagName === "SCRIPT" && node?.src != null) {
+        if (node.src.includes("economy.js")) {
+          let overwritenFunction = document.createElement("script");
+          overwritenFunction.type = "text/javascript";
+          overwritenFunction.src = chrome.runtime.getURL("js/OverwrittenFunction.js");
+          document.head.appendChild(overwritenFunction);
+        } else if (node.src.includes("shared_responsive_adapter")) {
+          let barRenderer = document.createElement("script");
+          barRenderer.type = "text/javascript";
+          barRenderer.src = chrome.runtime.getURL("js/barRenderer.js");
+          document.head.appendChild(barRenderer);
+          observer.disconnect();
         }
-    })
-})
+      }
+    }
+  });
+});
 
 observer.observe(document, {
-    childList: true
-    , subtree: true
-    , attributes: false
-    , characterData: false
-})
+  childList: true,
+  subtree: true,
+  attributes: false,
+  characterData: false,
+});
